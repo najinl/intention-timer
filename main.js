d737983606b4a47ce68743fde9e888614c5e4a81
Logan Vincent <ldvsimp@gmail.com>
2021-07-29 19:00:20 -0600

    Add function to restrict charaacters from input

--- main.js
+++ main.js
@@ -2,13 +2,31 @@ var activityBtns = document.querySelector('.actv-button-row');
 var studyBtn = document.querySelector('#study-button');
 var meditateBtn = document.querySelector('#meditate-button');
 var exerciseBtn = document.querySelector('#exercise-button');
+var inputFields = document.querySelector('.time');
+
+var invalidCharacters = [
+  '-',
+  '+',
+  'e',
+];
 
 activityBtns.addEventListener('click', displayActiveActivity);
+inputFields.addEventListener('keydown', function(e) {
+//   function eNo(e) {
+//   var noE = ["e", "E"];
+//   if (noE.includes(e.key)) {
+//     event.preventDefault();
+//   }
+// }
+  if (invalidCharacters.includes(e.key)) {
+    e.preventDefault();
+  }
+})
 
 
 
-function displayActiveActivity(event) {
-  event.preventDefault();
+function displayActiveActivity(e) {
+  e.preventDefault();
   var btnID = event.target.id;
   if (btnID === "study-button") {
     event.target.className += ' active-study';
